5fa1f505b4d69f898da871841bbdbfef7e54314e
mariafer057 <mariafernandaalves057@gmail.com>
2024-11-07 15:09:49 -0400

    07/11

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,8 @@
 'use client';
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import { useRouter } from 'next/navigation';
+import { ApiURL } from '@/config';
+import { setCookie } from 'nookies';
 import Link from 'next/link';
 
 import './login.css';
@@ -8,41 +10,84 @@ import './login.css';
 export default function Login() {
     const router = useRouter();
     const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
+    const [password, setPassword] = useState('');
     const [errologin, setErrologin] = useState('');
-
+    interface ResponseSignin {
+        erro: boolean,
+        mensagem: string,
+        token?: string
+      }
     const login = () => {
-        if (email.endsWith('admin@gmail.com') && senha === 'admin') {
+        if (email.endsWith('admin@gmail.com') && password === 'admin') {
             router.push('/');
         } else {
             setErrologin('Email e/ou senha incorretos :(');
         }
     };
+    
+    const  handleSubmit = async (e : FormEvent) => {
+        e.preventDefault();
+        try {
+         const response = await fetch(`${ApiURL}/auth/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type' : 'application/json'
+          },
+          body: JSON.stringify({email, password})
+         })
+          if (response){
+            const data : ResponseSignin = await response.json()
+            const {erro, mensagem, token = ''} = data;
+            console.log(data)
+            if (erro){
+              setError(mensagem)
+            } else {
+              // npm i nookies setCookie
+              setCookie(undefined, 'restaurant-token', token, {
+                maxAge: 60*60*1 // 1 hora
+              } )
+  
+            }
+          } else {
+  
+          }
+      } 
+       catch (error) {
+      console.error('Erro na requisicao', error)
+    }
+  
+        console.log('Email:',  email);
+        console.log('Senha:', password);
+    };
+    
 
     return (
         <div>
             <center>
                 <h1>PÁGINA DE LOGIN</h1>
                 <br />
-                <input 
-                    type="email" 
-                    placeholder="Digite seu email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <br />
-                <br />
-                <input 
-                    type="password" 
-                    placeholder="Digite sua senha" 
-                    value={senha} 
-                    onChange={(e) => setSenha(e.target.value)}
-                />
-                <br />
-                <br />
-                <button onClick={login}>Login</button>       
-                {errologin && <p style={{ color: 'red' }}>{errologin}</p>}
-                <br />
+                <form onSubmit={HandleSubmit}>
+                    <input 
+                        type="email" 
+                        placeholder="Digite seu email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <input 
+                        type="password" 
+                        placeholder="Digite sua senha" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <br />
+                    <br />
+                    <button type='submit'>Login</button>       
+                    {errologin && <p style={{ color: 'red' }}>{errologin}</p>}
+                    <br />
+
+                </form>
                 <button>
                     <Link href={"/cadastro/"}>
                     <p>Faça o cadastro aqui</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { parseCookie} from 'nookies';
 
+const router = useRouter();
+useEffect(() => {
+  const {'restaurant-token' : token} = parseCookies()
+  if (!token){
+    router.push('/login')
+  }
+}, [])
 export default function Home() {
   return (
     <div>
